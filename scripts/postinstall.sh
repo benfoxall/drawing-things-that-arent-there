1e154eb12cd62ac919807d04e5a8ed12d8e1747c
Ben Foxall <benfoxall@gmail.com>
2017-06-12 11:23:10 +0100

    Talk dependencies
    Testing out using an install script to copy in ./script/postinstall.sh
    
    Not sure of a nice minimal way of using dependencies without 
    a full-on workflow.

---
+++ scripts/postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p talk/lib
+
+cp -r node_modules/reveal.js talk/lib/reveal.js
+cp -r node_modules/slide-builder/slide-builder.js talk/lib/slide-builder.js
+cp -r node_modules/localforage/dist/localforage.js talk/lib/localforage.js
+cp -r node_modules/gl-matrix/dist/gl-matrix.js talk/lib/gl-matrix.js
